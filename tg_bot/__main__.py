873f2df7263af91012a7018a3da2553d905b68a5
JeffreyTackett <35301380+JeffreyTackett@users.noreply.github.com>
2019-04-12 14:57:40 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -50,8 +50,7 @@ And the following:
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 It took lots of work for [my creator](t.me/surajsharma007) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!)."""
+motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer."""
 
 IMPORTED = {}
 MIGRATEABLE = []
